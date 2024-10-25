e3a01e5d34097481d5ea05f42c8e8e4e933024d5
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-10-25 11:26:26 -0300

    feat: add initial product dropdown menu

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -3,11 +3,21 @@
 import { Badge } from "@/app/_components/ui/badge"
 import { Product } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-import { CircleIcon } from "lucide-react"
+import { CircleIcon, ClipboardCopyIcon, EditIcon, MoreHorizontalIcon, TrashIcon } from "lucide-react"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu"
+import { Button } from "@/app/_components/ui/button"
 
 
 const getStatusLabel = (status: string) => {
-  if(status === "IN_STOCK") {
+  if (status === "IN_STOCK") {
     return "Em estoque"
   }
 
@@ -31,14 +41,49 @@ export const productsTableColumns: ColumnDef<Product>[] = [
     accessorKey: "status",
     header: "Status",
     cell: (row) => {
-        const product = row.row.original
-        const label = getStatusLabel(product.status)
-        return(
-          <Badge variant={label === "Em estoque" ? "default": "outline"} className="gap-2">
-            <CircleIcon className={`${label === "Em estoque" ? "fill-primary-foreground" : "fill-destructive-foreground"}`} size={14}/>
-            {label}
-          </Badge>
-        )
+      const product = row.row.original
+      const label = getStatusLabel(product.status)
+      return (
+        <Badge variant={label === "Em estoque" ? "default" : "outline"} className="gap-2">
+          <CircleIcon className={`${label === "Em estoque" ? "fill-primary-foreground" : "fill-destructive-foreground"}`} size={14} />
+          {label}
+        </Badge>
+      )
     }
   },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: (row) => {
+
+      const product = row.row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"}>
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="gap-1.5" onClick={() => navigator.clipboard.writeText(product.id)}>
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-1.5">
+              <EditIcon size={16} />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-1.5">
+              <TrashIcon size={16} />
+              Deletar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+      )
+    }
+  }
 ]
