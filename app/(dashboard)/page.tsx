54f971778a0a0a85311f1f5e6a8bccced3d0b9b2
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-11-21 13:23:28 -0300

    feat: add total revenue component with streaming

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,10 +5,13 @@ import { getDashboard } from "../_data-acess/dashboard/get-dashboard";
 import { formatCurrency } from "../_helpers/currency";
 import RevenueChart from "./_components/revenue-chart";
 import MostSoldProductItem from "./_components/most-sold-product-item";
+import TotalRevenueCard from "./_components/total-revenue-card";
+import { Suspense } from "react";
+import { Skeleton } from "../_components/ui/skeleton";
 
 export default async function Home() {
 
-  const { totalRevenue, todayRevenue, totalSales, totalStock, totalProducts, totalLast14DaysRevenue, mostSoldProducts } = await getDashboard()
+  const { todayRevenue, totalSales, totalStock, totalProducts, totalLast14DaysRevenue, mostSoldProducts } = await getDashboard()
 
   return (
     <div className="m-8 w-full space-y-8 rounded-lg flex flex-col">
@@ -20,14 +23,9 @@ export default async function Home() {
       </Header>
 
       <div className="grid grid-cols-2 gap-6">
-        <SummaryCard>
-          <SummaryCardIcon>
-            <DollarSign />
-          </SummaryCardIcon>
-
-          <SummaryCardTitle>Receita Total</SummaryCardTitle>
-          <SummaryCardValue>{formatCurrency(totalRevenue)}</SummaryCardValue>
-        </SummaryCard>
+        <Suspense fallback={<Skeleton className="bg-white bg-opacity-75 rounded-xl" />}>
+          <TotalRevenueCard />
+        </Suspense>
 
         <SummaryCard>
           <SummaryCardIcon>
--- app/_data-acess/dashboard/get-dashboard.ts
+++ app/_data-acess/dashboard/get-dashboard.ts
@@ -18,7 +18,6 @@ export interface MostSoldProductDto {
 }
 
 interface DashboardDto {
-  totalRevenue: number;
   todayRevenue: number;
   totalSales: number;
   totalStock: number;
@@ -53,11 +52,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
       totalRevenue: dayTotalRevenue[0].totalRevenue,
     });
   }
-  const totalRevenueQuery = `
-  SELECT SUM("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "totalRevenue"
-  FROM "SaleProduct"
-  JOIN "Sale" ON "SaleProduct"."saleId" = "Sale"."id";
-`;
   const todayRevenueQuery = `
   SELECT SUM("SaleProduct"."unitPrice" * "SaleProduct"."quantity") as "todayRevenue"
   FROM "SaleProduct"
@@ -66,8 +60,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
 `;
   const startOfDay = new Date(new Date().setHours(0, 0, 0, 0));
   const endOfDay = new Date(new Date().setHours(23, 59, 59, 999));
-  const totalRevenuePromise =
-    db.$queryRawUnsafe<{ totalRevenue: number }[]>(totalRevenueQuery);
   const todayRevenuePromise = db.$queryRawUnsafe<{ todayRevenue: number }[]>(
     todayRevenueQuery,
     startOfDay,
@@ -99,16 +91,13 @@ export const getDashboard = async (): Promise<DashboardDto> => {
       price: number;
     }[]
   >(mostSoldProductsQuery);
-
   const [
-    totalRevenue,
     todayRevenue,
     totalSales,
     totalStock,
     totalProducts,
     mostSoldProducts,
   ] = await Promise.all([
-    totalRevenuePromise,
     todayRevenuePromise,
     totalSalesPromise,
     totalStockPromise,
@@ -116,7 +105,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     mostSoldProductsPromise,
   ]);
   return {
-    totalRevenue: totalRevenue[0].totalRevenue,
     todayRevenue: todayRevenue[0].todayRevenue,
     totalSales,
     totalStock: Number(totalStock._sum.stock),
