4c4d7400b1af40481ed1384be33d2293fd14df0f
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-11-22 16:45:39 -0300

    feat: add last 14 days revenue card with streaming

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,6 @@
 import Header, { HeaderLeft, HeaderSubtitle, HeaderTitle } from "../_components/header";
 import { SummaryCardSkeleton } from "./_components/summary-card";
 import { getDashboard } from "../_data-acess/dashboard/get-dashboard";
-import RevenueChart from "./_components/revenue-chart";
 import MostSoldProductItem from "./_components/most-sold-product-item";
 import TotalRevenueCard from "./_components/total-revenue-card";
 import { Suspense } from "react";
@@ -9,10 +8,12 @@ import TodayRevenueCard from "./_components/today-revenue-card";
 import TotalSalesCard from "./_components/total-sales-card";
 import TotalInStockCard from "./_components/total-in-stock-card";
 import TotalProductsCard from "./_components/total-products-card";
+import LastDaysRevenueCard from "./_components/last-14-days-revenue-card";
+import { Skeleton } from "../_components/ui/skeleton";
 
 export default async function Home() {
 
-  const { totalLast14DaysRevenue, mostSoldProducts } = await getDashboard()
+  const { mostSoldProducts } = await getDashboard()
 
   return (
     <div className="m-8 w-full space-y-8 rounded-lg flex flex-col">
@@ -48,11 +49,15 @@ export default async function Home() {
       </div>
 
       <div className="grid min-h-0 grid-cols-[minmax(0,2.5fr),minmax(0,1fr)] gap-4">
-        <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white p-6">
-          <p className="text-lg font-semibold text-slate-900">Receita</p>
-          <p className="text-sm text-slate-400">Últimos 14 dias</p>
-          <RevenueChart data={totalLast14DaysRevenue} />
-        </div>
+        <Suspense fallback={
+          <Skeleton className="bg-white p-6">
+            <div className="space-y-2">
+              <div className="h-5 w-[86.26px] rounded-md bg-gray-200" />
+              <div className="h-8 w-48 rounded-md bg-gray-200" />
+            </div>
+          </Skeleton>}>
+          <LastDaysRevenueCard />
+        </Suspense>
 
         <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white">
           <p className="text-lg font-semibold text-slate-900 p-6">Produtos mais vendidos</p>
